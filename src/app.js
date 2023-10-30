a603fbd3ea84872deb3d27be6a4a8d3a08089657
marco perez <mp4667596@gmail.com>
2023-10-30 10:11:32 -0300

    avance

--- src/app.js
+++ src/app.js
@@ -5,10 +5,8 @@ const router = require('./routes');
 const errorHandler = require('./utils/errorHandler');
 require('dotenv').config();
 
-// Esta es nuestra aplicación
 const app = express();
 
-// Middlewares 
 app.use(express.json());
 app.use(helmet({
     crossOriginResourcePolicy: false,
@@ -20,7 +18,6 @@ app.get('/', (req, res) => {
     return res.send("Welcome to express!");
 })
 
-// middlewares después de las rutas
 app.use(errorHandler)
 
 module.exports = app;
